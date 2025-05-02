854dc225074059abca563fd67dfe4e4aee07b0a1
Emmanuel-Etukudo <etukzbong@gmail.com>
2025-05-02 05:55:48 +0100

    feat: show check icon first on complete then task icon

--- projects/habit_tracker_flutter/lib/ui/task/animated_task.dart
+++ projects/habit_tracker_flutter/lib/ui/task/animated_task.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:habit_tracker_flutter/constants/app_assets.dart';
 import 'package:habit_tracker_flutter/ui/common_widgets/centered_svg_icon.dart';
 import 'package:habit_tracker_flutter/ui/task/task_completion_ring.dart';
 import 'package:habit_tracker_flutter/ui/theming/app_theme.dart';
@@ -15,6 +16,7 @@ class _AnimatedTaskState extends State<AnimatedTask>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animatedController;
   late final Animation<double> _curveAnimation;
+  bool _showCheckIcon = false;
 
   @override
   void initState() {
@@ -23,12 +25,27 @@ class _AnimatedTaskState extends State<AnimatedTask>
         vsync: this, duration: const Duration(milliseconds: 750));
     _curveAnimation =
         _animatedController.drive(CurveTween(curve: Curves.easeInOut));
+    _animatedController.addStatusListener(_checkStatusUpdates);
   }
 
   @override
   void dispose() {
+    _animatedController.removeStatusListener(_checkStatusUpdates);
     _animatedController.dispose();
     super.dispose();
+  }
+
+  void _checkStatusUpdates(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      if (mounted) {
+        setState(() => _showCheckIcon = true);
+      }
+      Future.delayed(const Duration(milliseconds: 1000), () {
+        if (mounted) {
+          setState(() => _showCheckIcon = false);
+        }
+      });
+    }
   }
 
   @override
@@ -49,7 +66,10 @@ class _AnimatedTaskState extends State<AnimatedTask>
                 TaskCompletionRing(progress: _curveAnimation.value),
                 Positioned.fill(
                   child: CenteredSvgIcon(
-                      iconName: widget.iconName, color: iconColor),
+                      iconName: hasCompleted && _showCheckIcon
+                          ? AppAssets.check
+                          : widget.iconName,
+                      color: iconColor),
                 )
               ],
             );
@@ -60,7 +80,7 @@ class _AnimatedTaskState extends State<AnimatedTask>
   void _handleTapDown(TapDownDetails details) {
     if (_animatedController.status != AnimationStatus.completed) {
       _animatedController.forward();
-    } else {
+    } else if (!_showCheckIcon) {
       _animatedController.value = 0.0;
     }
   }
