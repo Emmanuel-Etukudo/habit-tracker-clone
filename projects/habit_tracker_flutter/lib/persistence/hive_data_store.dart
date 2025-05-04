706eb72cd8e4619354289877b7b647bb141849c3
Emmanuel-Etukudo <etukzbong@gmail.com>
2025-05-04 06:01:42 +0100

    chore: creating taskstate hive model class

--- projects/habit_tracker_flutter/lib/persistence/hive_data_store.dart
+++ projects/habit_tracker_flutter/lib/persistence/hive_data_store.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:habit_tracker_flutter/models/task.dart';
+import 'package:habit_tracker_flutter/models/task_state.dart';
 import 'package:hive_flutter/adapters.dart';
 
 class HiveDataStore {
   static const taskBoxName = 'tasks';
+  static const taskStateBoxName = 'taskState';
+
+  static String taskStateKey(String taskId) {
+    return 'taskState/$taskId';
+  }
+
   Future<void> init() async {
     await Hive.initFlutter();
 
-    // Register the adapter for the Task model
+    // Register the adapter
     Hive.registerAdapter<Task>(TaskAdapter());
+    Hive.registerAdapter<TaskState>(TaskStateAdapter());
 
     // Open the box for storing tasks
     await Hive.openBox<Task>(taskBoxName);
+    await Hive.openBox<TaskState>(taskStateBoxName);
   }
 
   Future<void> createDemoTasks(
@@ -29,6 +38,21 @@ class HiveDataStore {
 
   ValueListenable<Box<Task>> taskListenable() {
     return Hive.box<Task>(taskBoxName).listenable();
+  }
+
+  Future<void> setTaskState({
+    required Task task,
+    required String completed,
+  }) async {
+    final box = Hive.box<TaskState>(taskStateBoxName);
+    final taskState = TaskState(taskId: task.id, completed: completed);
+    await box.put(taskStateKey(task.id), taskState);
+  }
+
+  ValueListenable<Box<TaskState>> taskStateListenable({required Task task}) {
+    final box = Hive.box<TaskState>(taskStateBoxName);
+    final key = taskStateKey(task.id);
+    return box.listenable(keys: [key]);
   }
 }
 
